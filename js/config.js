934025e718fd4fa1aa7fc73cfc0c6341e3a3ff64
Kashif Ali <37431647+kf007@users.noreply.github.com>
2025-11-08 17:46:01 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,8 +5,9 @@ window._config = {
         region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://c7ceyjya6a.execute-api.ap-south-1.amazonaws.com/Dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
