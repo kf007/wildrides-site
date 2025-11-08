0fd2c54e870f0cf0ddb4999351469857166de87a
Kashif Ali <37431647+kf007@users.noreply.github.com>
2025-11-08 17:21:13 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,11 +1,12 @@
 window._config = {
     cognito: {
         userPoolId: 'ap-south-1_2sjVbWdPl', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: 'je48o2dq0urg1qqcposikstk0', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: 'rupu8aq9a3c6dsp4gdfi2s1jj', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
