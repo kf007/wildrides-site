2dcf417c74c417a275a9b07214c6b8a61a9e3e32
Kashif Ali <37431647+kf007@users.noreply.github.com>
2025-11-09 19:20:25 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,13 +1,14 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-south-1_WzF8lBzUg', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '4dpcct1e9uh9d0l4tmor6o2efa', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'ap-south-1_qfTolDGaa', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6jlhce9qfum6oa3gr4o4k1dpc8', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
 
